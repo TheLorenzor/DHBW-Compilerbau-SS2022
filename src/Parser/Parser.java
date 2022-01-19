b9af6c7643a52a97f73d236d71bccf1b3887a1c1
The Loraxxz <tilmann.lorenz@mail.com>
2022-01-19 16:51:19 +0100

    Skelett für Parser fertig

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -5,7 +5,35 @@ public class Parser {
     private final String eingabe;
 
     public Parser(String eingabe) {
-        this.eingabe=eingabe;
-        this.position=0;
+        this.eingabe = eingabe;
+        this.position = 0;
+    }
+
+    public Visitable start(Visitable parameter) {
+
+        return null;
+    }
+
+    private Visitable alphanum(Visitable parameter) {
+        return null;
+    }
+
+    private void match(char symbol) {
+        if ((eingabe == null) || ("".equals(eingabe))) {
+            throw new RuntimeException("Syntax error !");
+        }
+        if (position >= eingabe.length()) {
+            throw new RuntimeException("End of input reached !");
+        }
+        if (eingabe.charAt(position) != symbol) {
+            throw new RuntimeException("Syntax error !");
+        }
+        position++;
+    }
+
+    private void assertEndOfInput() {
+        if (position < eingabe.length()) {
+            throw new RuntimeException(" No end of input reached !");
+        }
     }
 }
