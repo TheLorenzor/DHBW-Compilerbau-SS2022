5e5654ea4e9d3c2390dd24dad02a614681ed5f5b
The Loraxxz <tilmann.lorenz@mail.com>
2022-01-19 16:36:54 +0100

    added Parser and readme.md file

---
+++ README.md
@@ -0,0 +1,2 @@
+#Beschreibung  
+Projekt für 4. Semester Compilerbau an der DHBW Mosbach --> Herr Der..... , F.
---
+++ src/Parser/Parser.java
@@ -0,0 +1,11 @@
+package Parser;
+
+public class Parser {
+    private int position;
+    private final String eingabe;
+
+    public Parser(String eingabe) {
+        this.eingabe=eingabe;
+        this.position=0;
+    }
+}
